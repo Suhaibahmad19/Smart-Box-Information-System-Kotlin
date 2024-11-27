157c14d3f050c430343c2aef94a18e7ba22b0b61
Muhammad Awais <muhammadawais3744@gmail.com>
2024-11-28 03:18:41 +0500

    Basic OTP Generator Prototype Basic Standard of SmartBOX Emulator

--- app/src/main/java/com/example/smartbox19nov/MainActivity.kt
+++ app/src/main/java/com/example/smartbox19nov/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.smartbox19nov
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -12,8 +12,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val intent = Intent(this, SmartBoxEmulator::class.java)
+        var parcels = ArrayList<PackageItem>()
+        var sbox = SmartBox("1234", 2, parcels,false)
+        intent.putExtra("SMART_BOX", sbox) // Pass Parcelable object
+        startActivity(intent)
         setContentView(R.layout.activity_login) // Start with the login layout
-
         setupLoginScreen() // Initialize login screen functionality
     }
 
